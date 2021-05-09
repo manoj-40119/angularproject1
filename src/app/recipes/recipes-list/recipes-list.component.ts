8345e69cca6afc17275790284f253893cb86e4cb
manoj-40119 <manojkumarbonu1999@gmail.com>
2021-05-09 19:35:01 +0530

    added somemore

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipes-list',
   templateUrl: './recipes-list.component.html',
   styleUrls: ['./recipes-list.component.css']
 })
-export class RecipesListComponent implements OnInit {
+export class RecipesListComponent  {
+  recipes : Recipe[] = [
+    new Recipe('Item Name','Sample Test','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2BFanf5H9xCJOl-IfN7GwSc68GO8M-Z38Ew&usqp=CAU')
+  ];
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
+
 
 }
